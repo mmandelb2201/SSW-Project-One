58a9594fb14aff30e82599b31cb4d1c84599cc74
Matthew Mandelbaum <mattrmandelbaum@gmail.com>
2021-03-06 17:46:26 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,44 @@
+/*
+---------HOW TO TEST WEBSITE------------
+1) run 'npm run dev in terminal'
+2) start debugging in IDE or
+open chrome and go to localhost:5000
+---------HOW TO STOP TESTING SERVER-------
+1) in  terminal type control c
+2) type Y and press enter
+*/
+//index.js handles set up of the site and adding node.js modules
+//add the node.js modules to the site
+const express = require('express');
+const path = require('path');
+const app = express();
+const handlebars = require('express-handlebars');
+//set handelbars as view engine
+app.engine('handlebars', handlebars(({
+    //set main as the header and footer for the site
+    defaultLayout: 'main',
+    //set view/layouts as our static page files
+    layoutsDir: path.join(__dirname, 'views/layouts')
+  })));
+app.set('view engine', 'handlebars');
+//set /scripts as our folder for javascript files
+app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
+//create a route for each page, might change to a seperate js file later
+//app.get is a request for the page ending in "/" or "", 
+//res is the respone, we use res.render to respond with a static page file
+//with variables
+app.get('/', (req, res) => {
+    res.render('index.handlebars', {
+        title: 'Home Page'
+    });
+});
+app.get('/about', (req, res) => {
+    res.render('about.handlebars', {
+        title: 'About Page'
+    });
+});
+
+//set the port to run the website on
+const PORT = process.env.PORT || 5000;
+//run the website on PORT
+app.listen(PORT, () => console.log('server started on port ' + PORT));
