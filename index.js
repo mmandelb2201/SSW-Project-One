2ba904d931aac038ff6adb9d0adb554502ad8034
Matthew Mandelbaum <mattrmandelbaum@gmail.com>
2021-03-26 09:54:08 -0400

    changed code to follow coding conventions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /*
 ---------HOW TO TEST WEBSITE------------
-1) run 'npm run dev in terminal'
+1) run "npm run dev in terminal"
 2) start debugging in IDE or
 open chrome and go to localhost:8080
 ---------HOW TO STOP TESTING SERVER-------
@@ -9,67 +9,67 @@ open chrome and go to localhost:8080
 */
 //index.js handles set up of the site and adding node.js modules
 //add the node.js modules to the site
-const express = require('express');
-const path = require('path');
-const app = express();
-const handlebars = require('express-handlebars');
+const express = require("express")
+const path = require("path")
+const app = express()
+const handlebars = require("express-handlebars")
 //set handlebars as view engine
-app.engine('handlebars', handlebars(({
+app.engine("handlebars", handlebars(({
     //set main as the header and footer for the site
-    defaultLayout: 'main',
+    defaultLayout: "main",
     //set view/layouts as our static page files
-    layoutsDir: path.join(__dirname, 'views/layouts')
-})));
-app.set('view engine', 'handlebars');
+    layoutsDir: path.join(__dirname, "views/layouts")
+})))
+app.set("view engine", "handlebars")
 //set /scripts as our folder for javascript files
-app.use('/scripts', express.static(path.join(__dirname, 'scripts')));
+app.use("/scripts", express.static(path.join(__dirname, "scripts")))
 //Serves static files (we need it to import a css file)
-app.use(express.static('public'));
+app.use(express.static("public"))
 //create a route for each page, might change to a separate js file later
 //app.get is a request for the page ending in "/" or "", 
 //res is the response, we use res.render to respond with a static page file
 //with variables
-app.get('/', (req, res) => {
-    res.render('index.handlebars', {
-        title: 'Study Buddy'
-    });
-});
-app.get('/about', (req, res) => {
-    res.render('about.handlebars', {
-        title: 'About Page'
-    });
-});
-
-app.get('/user/new', (req, res) => {
-    res.render('newuser.handlebars', {
-        title: 'Create Account'
+app.get("/", (req, res) => {
+    res.render("index.handlebars", {
+        title: "Study Buddy"
     })
-});
-app.get('/question/show/:id', (req, res) => {
-    res.render('question.handlebars', {
-        title: 'Question',
+})
+app.get("/about", (req, res) => {
+    res.render("about.handlebars", {
+        title: "About Page"
+    })
+})
+
+app.get("/user/new", (req, res) => {
+    res.render("newuser.handlebars", {
+        title: "Create Account"
+    })
+})
+app.get("/question/show/:id", (req, res) => {
+    res.render("question.handlebars", {
+        title: "Question",
         question: req.params.id
-    });
-});
-app.get('/question/new', (req, res) => {
-    res.render('newquestion.handlebars', {
-        title: 'New Question'
-    });
-});
-app.get('/account/sign-in', (req, res) => {
-    res.render('signin.handlebars', {
-        title: 'Sign In'
-    });
-});
-app.get('/account/show/:id', (req, res) => {
-    res.render('account.handlebars', {
-        title: 'Account',
+    })
+})
+app.get("/question/new", (req, res) => {
+    res.render("newquestion.handlebars", {
+        title: "New Question"
+    })
+})
+app.get("/account/sign-in", (req, res) => {
+    res.render("signin.handlebars", {
+        title: "Sign In"
+    })
+})
+app.get("/account/show/:id", (req, res) => {
+    res.render("account.handlebars", {
+        title: "Account",
         userID: req.params.id
-    });
-});
+    })
+})
 //set the port to run the website on
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 //run the website on PORT
-app.listen(PORT, () => console.log('server started on port ' + PORT));
+app.listen(PORT, () => console.log("server started on port " + PORT))
 
 /**/
