7ab501d61a756257b3fa275d8fb1b3d6f7ebcdeb
“Mikayla <mikayla.mount22@gmail.comgit config --global user.email mikayla.mount22@gmail.com>
2021-05-03 16:38:35 -0400

    FUCJK

--- public/cards.js
+++ public/cards.js
@@ -3,3 +3,13 @@ const card = document.querySelector('.cardInner')
 card.addEventListener('click', function() {
     card.classList.toggle('isFlipped')
 })
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
